5845b7e96df1e7e3f25b0e2cf903c45f9c4e706a
Geobarden <zhubojun@GBDdeMacBook-Pro.local>
2017-11-12 21:50:55 +0800

    init lab5

---
+++ lab5/lock_protocol.h
@@ -0,0 +1,21 @@
+// lock protocol
+
+#ifndef lock_protocol_h
+#define lock_protocol_h
+
+#include "rpc.h"
+
+class lock_protocol {
+ public:
+  enum xxstatus { OK, RETRY, RPCERR, NOENT, IOERR };
+  typedef int status;
+  typedef unsigned long long lockid_t;
+  enum lock_status {LOCKED, UNLOCKED};
+  enum rpc_numbers {
+    acquire = 0x7001,
+    release,
+    stat
+  };
+};
+
+#endif 
